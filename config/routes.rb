f9e740226361d2c09112760905f042a1fe3bc52d
Drew Dara-Abrams <dda@dara-abrams.com>
2010-05-23 11:37:19 -0700

    moving api to /api/v1

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 ActionController::Routing::Routes.draw do |map|
   map.namespace(:api) do |api|
-    api.resources :users, 
-                  :travel_fixes, 
-                  :landmarks, 
-                  :landmark_visits, 
-                  :direction_distance_estimates, 
-                  :regions, 
-                  :log_entries
+    api.namespace(:v1) do |v1|
+      v1.resources :users, 
+                   :travel_fixes, 
+                   :landmarks, 
+                   :landmark_visits, 
+                   :direction_distance_estimates, 
+                   :regions, 
+                   :log_entries
+    end
   end
   
   map.devise_for :users
