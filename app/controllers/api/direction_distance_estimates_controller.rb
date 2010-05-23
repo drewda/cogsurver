9e6a5dea266845f23b3f09c723abe3492acc3b53
Drew Dara-Abrams <dda@dara-abrams.com>
2010-05-23 13:17:34 -0700

    work on api routes and controllers

--- app/controllers/api/direction_distance_estimates_controller.rb
+++ app/controllers/api/direction_distance_estimates_controller.rb
@@ -1,16 +1,13 @@
 class Api::DirectionDistanceEstimatesController < ApplicationController
-  # GET /direction_distance_estimates
   # GET /direction_distance_estimates.xml
   def index
     @direction_distance_estimates = DirectionDistanceEstimate.all
 
     respond_to do |format|
-      format.html # index.html.erb
       format.xml  { render :xml => @direction_distance_estimates }
     end
   end
 
-  # GET /direction_distance_estimates/1
   # GET /direction_distance_estimates/1.xml
   def show
     @direction_distance_estimate = DirectionDistanceEstimate.find(params[:id])
@@ -21,23 +18,6 @@ class Api::DirectionDistanceEstimatesController < ApplicationController
     end
   end
 
-  # GET /direction_distance_estimates/new
-  # GET /direction_distance_estimates/new.xml
-  def new
-    @direction_distance_estimate = DirectionDistanceEstimate.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.xml  { render :xml => @direction_distance_estimate }
-    end
-  end
-
-  # GET /direction_distance_estimates/1/edit
-  def edit
-    @direction_distance_estimate = DirectionDistanceEstimate.find(params[:id])
-  end
-
-  # POST /direction_distance_estimates
   # POST /direction_distance_estimates.xml
   def create
     @direction_distance_estimate = DirectionDistanceEstimate.new(params[:direction_distance_estimate])
@@ -45,16 +25,13 @@ class Api::DirectionDistanceEstimatesController < ApplicationController
     respond_to do |format|
       if @direction_distance_estimate.save
         flash[:notice] = 'DirectionDistanceEstimate was successfully created.'
-        format.html { redirect_to(@direction_distance_estimate) }
-        format.xml  { render :xml => @direction_distance_estimate, :status => :created, :location => @direction_distance_estimate }
+        format.xml  { render :xml => @direction_distance_estimate, :status => :created, :location => api_direction_distance_estimate_url(@direction_distance_estimate) }
       else
-        format.html { render :action => "new" }
         format.xml  { render :xml => @direction_distance_estimate.errors, :status => :unprocessable_entity }
       end
     end
   end
 
-  # PUT /direction_distance_estimates/1
   # PUT /direction_distance_estimates/1.xml
   def update
     @direction_distance_estimate = DirectionDistanceEstimate.find(params[:id])
@@ -62,23 +39,19 @@ class Api::DirectionDistanceEstimatesController < ApplicationController
     respond_to do |format|
       if @direction_distance_estimate.update_attributes(params[:direction_distance_estimate])
         flash[:notice] = 'DirectionDistanceEstimate was successfully updated.'
-        format.html { redirect_to(@direction_distance_estimate) }
         format.xml  { head :ok }
       else
-        format.html { render :action => "edit" }
         format.xml  { render :xml => @direction_distance_estimate.errors, :status => :unprocessable_entity }
       end
     end
   end
 
-  # DELETE /direction_distance_estimates/1
   # DELETE /direction_distance_estimates/1.xml
   def destroy
     @direction_distance_estimate = DirectionDistanceEstimate.find(params[:id])
     @direction_distance_estimate.destroy
 
     respond_to do |format|
-      format.html { redirect_to(direction_distance_estimates_url) }
       format.xml  { head :ok }
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,12 @@
 ActionController::Routing::Routes.draw do |map|
   map.namespace(:api) do |api|
-    api.namespace(:v1) do |v1|
-      v1.resources :users, 
-                   :travel_fixes, 
-                   :landmarks, 
-                   :landmark_visits, 
-                   :direction_distance_estimates, 
-                   :regions, 
-                   :log_entries
-    end
+    api.resources :users, 
+                  :travel_fixes, 
+                  :landmarks, 
+                  :landmark_visits, 
+                  :direction_distance_estimates, 
+                  :regions, 
+                  :log_entries
   end
   
   map.devise_for :users
