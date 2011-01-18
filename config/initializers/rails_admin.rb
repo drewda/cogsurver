1c79689167815825b95720020bfa272ee742bec9
Drew Dara-Abrams <dda@dara-abrams.com>
2011-01-17 17:28:12 -0800

    rails_admin pare down list of items

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -5,3 +5,15 @@ module RailsAdmin
     filter_access_to :all
   end
 end
+
+RailsAdmin.config do |config|
+  config.model User do
+    field :first_name
+    field :last_name
+    field :email
+    field :password
+    field :password_confirmation
+    field :travel_log_service_enabled
+    field :travel_log_service_interval
+  end
+end
