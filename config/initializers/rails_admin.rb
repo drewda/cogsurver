851145988558c4096a9faa12a5dbc1a433417312
Drew Dara-Abrams <drew@cog.(none)>
2011-01-25 16:47:37 -0800

    rails_admin accept longer travel log service interval

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -8,12 +8,25 @@ end
 
 RailsAdmin.config do |config|
   config.model User do
+    object_label do
+      "#{bindings[:object].full_name}"
+    end
     field :first_name
     field :last_name
     field :email
     field :password
     field :password_confirmation
     field :travel_log_service_enabled
-    field :travel_log_service_interval
+    field :travel_log_service_interval do
+      length 8
+    end
+  end
+  config.model Landmark do
+    edit do
+      field :name
+      field :user
+      field :latitude
+      field :longitude
+    end
   end
 end
